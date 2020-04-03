fbb2f55bccb66474906c749a047bc78aec83b36e
Lars Holdaas <larsholdaas@LarsnoMacBook-Pro.local>
2020-04-03 17:06:13 +0900

    changed primes$ to keep track of observed primes outside itself

--- creators/primes$.ts
+++ creators/primes$.ts
@@ -1,19 +1,27 @@
 import { Observable, Observer } from 'rxjs';
 
+let observedOddPrimes: number[] = [ 3 ];
+
 export const primes$: Observable<number> = Observable.create(
   (observer: Observer<number>) => {
-    const observedOddPrimes: number[] = [];
+    let outIndex = 0;
     observer.next(2);
-    let candidatePrime = 3;
+    outIndex++;
+    observer.next(3);
     while(true) {
-      observer.next(candidatePrime);
       if (observer.closed) {
         break;
       }
-      observedOddPrimes.push(candidatePrime);
-      candidatePrime += 2;
-      while(observedOddPrimes.some(observedPrime => candidatePrime % observedPrime === 0 )) {
-        candidatePrime += 2;
+      outIndex++;
+      if (outIndex <= observedOddPrimes.length - 1) {
+        observer.next(observedOddPrimes[outIndex]);
+      } else {
+        let candidatePrime = observedOddPrimes[observedOddPrimes.length - 1] + 2;
+        while(observedOddPrimes.some(observedPrime => candidatePrime % observedPrime === 0 )) {
+          candidatePrime += 2;
+        }
+        observedOddPrimes.push(candidatePrime);
+        observer.next(candidatePrime);
       }
     }
   }

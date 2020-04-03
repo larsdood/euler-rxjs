891dbfa479376af5aa9050b9e1d573792510bbb2
Lars Holdaas <larsholdaas@LarsnoMacBook-Pro.local>
2020-04-03 17:06:56 +0900

    write an increased primeFactors method (from 8 seconds to 3ms on task3 wow)

--- operators/primeFactors.ts
+++ operators/primeFactors.ts
@@ -1,5 +1,5 @@
 import { UnaryFunction, pipe, Observable } from 'rxjs';
-import { filter, map, reduce, switchMap, takeWhile } from 'rxjs/operators';
+import { filter, map, reduce, switchMap, takeWhile, tap } from 'rxjs/operators';
 import { primes$ } from '../creators/primes$';
 
 export const primeFactors = (): UnaryFunction<Observable<number>, Observable<number[]>> =>
@@ -8,6 +8,19 @@ export const primeFactors = (): UnaryFunction<Observable<number>, Observable<num
       takeWhile(prime => prime <= Math.sqrt(product)),
       filter(prime => product % prime === 0),
       reduce((accumulatedFactors: number[], primeFactor: number) => [...accumulatedFactors, primeFactor], [] as number[])
+    )),
+    map(val => val as number[])
+  );
+
+export const primeFactorsImproved = (): UnaryFunction<Observable<number>, Observable<number[]>> =>
+  pipe(
+    switchMap((product: number) => primes$.pipe(
+      takeWhile(prime => prime <= product),
+      filter(prime => product % prime === 0),
+      tap((prime) => {
+        product = product / prime;
+      }),
+      reduce((accumulatedFactors: number[], primeFactor: number) => [...accumulatedFactors, primeFactor], [] as number[]),
     )),
     map(val => val as number[])
   );
